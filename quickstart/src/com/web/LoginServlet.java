6f708c25ffb358d07f67618cbfd737b3c15babe2
xiaotaoqi <2536015980@qq.com>
2019-09-28 03:43:14 +0800

    两个模块补推

--- quickstart/src/com/web/LoginServlet.java
+++ quickstart/src/com/web/LoginServlet.java
@@ -13,18 +13,28 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //处理请求乱码
         req.setCharacterEncoding("UTF-8");
-
+        //通过req请求参数获取前台表单
         String username =req.getParameter("username");
         String password =req.getParameter("password");
 
+
+//        跳转之前把用户存入req对象
 //        req.setAttribute("username",username);
 //        req.setAttribute("password",password);
+//        通过req跳转
 //        req.getRequestDispatcher("/index.jsp").forward(req,resp);
 
+
+
+        //通过req请求获取session会话对象
         HttpSession session = req.getSession();
+        //把用户名存入session
         session.setAttribute("username",username);
+        //把密码存入session
         session.setAttribute("password",password);
+        //通过resp跳转
         resp.sendRedirect("/index.jsp");
 
 
